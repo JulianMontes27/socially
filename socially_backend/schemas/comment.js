ae37db1a5aa707385c862baa8569966c03477d60
Julian Montes <julianmontesPS4@gmail.com>
2022-10-21 00:34:55 -0500

    Sanity backend done

---
+++ socially_backend/schemas/comment.js
@@ -0,0 +1,20 @@
+export default{
+    name:'comment',
+    title: 'Comment',
+    type: 'document',
+    fields:[
+        
+        {
+            title: 'postedBy',
+            name:'Postedby',
+            type: 'postedBy',
+        
+        },
+        {
+            name:'comment',
+            title: 'Comment',
+            type: 'string',
+            
+        },
+    ]
+}
---
+++ socially_backend/schemas/save.js
@@ -0,0 +1,20 @@
+export default{
+    name:'save',
+    title: 'Save',
+    type: 'document',
+    fields:[
+        
+        {
+            title: 'postedBy',
+            name:'Postedby',
+            type: 'postedBy',
+        
+        },
+        {
+            name:'userId',
+            title: 'UserId',
+            type: 'string',
+            
+        },
+    ]
+}
